495192f4775b0807bcf7a5435b3236e71e028c99
sacrown <104371963+sacrown@users.noreply.github.com>
2022-06-25 11:56:50 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Lawal Saheed Adeolu')
+console.log('Height: 176cm');
+console.log('Country: Nigeria');
